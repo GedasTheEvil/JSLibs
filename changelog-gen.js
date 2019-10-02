b7f163f2fce8fcc2e9e8bb2ba303c39dcc9d6ce5
Gediminas Skucas <gediminas.skucas@gmail.com>
2019-10-02 15:40:43 +0300

    Update changelog-gen.js

--- changelog-gen.js
+++ changelog-gen.js
@@ -1,12 +1,11 @@
 function getChangeLog() {
 
   const list = document.querySelectorAll(`.commit-message.pr-1.flex-auto`)
-
   const doesNotHave = (text, phrase) => text.toLowerCase().search(phrase) === -1
 
   return Array.prototype.map.call(list, e => e.innerText)
     .filter(t => doesNotHave(t, 'merge pull request'))
     .filter(t => doesNotHave(t, 'built assets for release'))
     .map(t => `* ${t}`)
-
+    .reduce((a, b) => `${a}\n${b}`, '')
 }
